b1dc704f501c84505f37fd80f80c73d9c8e1a029
Patrick de Matos <65696357+patrickdmatos@users.noreply.github.com>
2024-04-27 23:39:48 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,26 +1,34 @@
+const readline = require('readline');
+
 function rankedCalculator(victories, defeats) {
     let media = victories - defeats
 
     if (media < 10) {
-        return {media: media, rank: 'Ferro'}
-    } else if(media <= 20){
-        return {media: media, rank: 'Bronze'}
+        return { media: media, rank: 'Ferro' }
+    } else if (media <= 20) {
+        return { media: media, rank: 'Bronze' }
     } else if (media <= 50) {
-        return {media: media, rank: 'Prata'}
+        return { media: media, rank: 'Prata' }
     } else if (media <= 80) {
-        return {media: media, rank: 'Ouro'}
+        return { media: media, rank: 'Ouro' }
     } else if (media <= 90) {
-        return {media: media, rank: 'Diamante'}
+        return { media: media, rank: 'Diamante' }
     } else if (media <= 100) {
-        return {media: media, rank: 'Lendário'}
+        return { media: media, rank: 'Lendário' }
     } else if (media >= 101) {
-        return {media: media, rank: 'Imortal'}
+        return { media: media, rank: 'Imortal' }
     }
 }
 
-let vic = 500
-let def = 412
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
 
-let rankLevel = rankedCalculator(vic, def)
-
-console.log("O Herói tem de saldo de " + rankLevel.media + " e está no nível " + rankLevel.rank)
+rl.question('Quantas vitórias você tem? ', (vic) => {
+    rl.question('E quantas vezes foi derrotado? ', (def) => {
+        let rankLevel = rankedCalculator(parseInt(vic), parseInt(def))
+        console.log("O Herói tem um saldo de " + rankLevel.media + " e está no nível " + rankLevel.rank)
+        rl.close();
+    });
+});
